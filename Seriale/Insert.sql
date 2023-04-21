34721f5748da548fd34350ae42fe5e6f1cc8dd06
PrzemyslawSzatkowski <131381499+PrzemyslawSzatkowski@users.noreply.github.com>
2023-04-21 02:27:35 +0200

    Create Insert.sql

---
+++ Seriale/Insert.sql
@@ -0,0 +1,32 @@
+INSERT INTO Kanał VALUES 
+	('TVP 1', 1952),
+	('TVP 2', 1970),
+	('POLSAT', 2000),
+	('TVN', 2002),
+	('TV Puls', 2005),
+	('TV Trwam', 2003);
+
+INSERT INTO Serial VALUES
+	('M jak Miłość', 2003, NULL, 2, 6),
+	('Ranczo', 2010, 2021, 2, 4),
+	('Świat według kiepskich', 1999, NULL, 3, 9),
+	('Przyjaciółki', 2012, NULL, 3, 8),
+	('Ślad', 2018, NULL, 3, 3),
+	('Na wspólnej', 2009, NULL, 4, 2),
+	('Chyłka', 2020, 2022, 4, 3);
+
+INSERT INTO Postać VALUES
+	('Hanka Mostowiak', 'T', 1),
+	('Marcin Mostowiak', 'T', 1),
+	('Ksiądz proboszcz', 'N', 2),
+	('Wójt', 'N', 2),
+	('Kłusy', 'T', 2),
+	('Lucy', 'T', 2),
+	('Ferdek Kiepski', 'N', 3),
+	('Babka Kiepska', 'T', 3),
+	('Edziu listonosz', 'T', 3),
+	('Arnold Boczek', 'T', 3),
+	('Cycu', 'N', 3),
+	('Mariolka', 'N', 3),
+	('Kamil Hoffer', 'N', 6),
+	('Marta Konarska', 'T', 6);

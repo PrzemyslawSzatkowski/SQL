7e9b6556bc9542701375704d2d548c9abb235b9e
PrzemyslawSzatkowski <131381499+PrzemyslawSzatkowski@users.noreply.github.com>
2023-04-21 02:26:27 +0200

    Create create.sql

---
+++ Seriale/create.sql
@@ -0,0 +1,24 @@
+CREATE DATABASE Seriale;
+USE Seriale;
+
+CREATE TABLE Kanał (
+	id_kanału int IDENTITY(1,1) PRIMARY KEY, 
+	nazwa varchar(40) NOT NULL, 
+	rok_powstania int NOT NULL
+);
+
+CREATE TABLE Serial (
+	id_serialu int IDENTITY(1,1) PRIMARY KEY,
+	nazwa varchar(40) NOT NULL,
+	rok_start int NOT NULL,
+	rok_koniec int,
+	id_kanału int NOT NULL FOREIGN KEY REFERENCES Kanał(id_kanału),
+	ocena int NOT NULL
+);
+
+CREATE TABLE Postać (
+	id_postaci int IDENTITY(1,1) PRIMARY KEY,
+	postać varchar(40) NOT NULL,
+	ginie char NOT NULL,
+	id_serialu int NOT NULL FOREIGN KEY REFERENCES Serial(id_serialu)
+);
